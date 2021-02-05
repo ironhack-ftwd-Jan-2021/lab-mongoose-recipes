3c1fe642c3adc4b0e7fd9313ef469ce9218f2a3a
ivan-user <username.web@protonmail.com>
2021-02-05 10:44:27 +0100

    Iteration 2

--- index.js
+++ index.js
@@ -21,7 +21,26 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    Recipe.create({
+        title: 'Uštipci',
+        level: 'Easy Peasy',
+        ingredients: [
+            '3 cups flour',
+            '2 eggs',
+            '1 tablespoon active dry yeast',
+            '1 teaspoon salt',
+            '1 cup warm milk',
+            '1 teaspoon sugar',
+            'Vegetable oil'],
+        cuisine: 'Croatian',
+        dishType: 'dessert',
+        image: 'https://www.196flavors.com/wp-content/uploads/2019/09/ustipci-1.jpg',
+        duration: 100,
+        creator: 'Slavic people'
+    }).then(recipe => { console.log('This recipe was created: ', recipe)})
+        .catch(err => { console.log('Recipe was not created! Error: ', err)})
+        
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+});
